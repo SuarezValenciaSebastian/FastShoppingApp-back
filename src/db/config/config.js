46755d3fd268fba3ef261501960b700320a6e556
Sebastián Suárez <suarez.valencia.sebastian@gmail.com>
2020-06-29 16:45:22 -0500

    Oops x2

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   development: {
-    username: "fastshoppingapp",
-    password: "0ilQLl6HVjvXsSEA",
-    database: "fast_shopping_app",
-    host: "testdb.sebastiansuarezvalencia.com",
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
     dialect: "mysql",
-  },
+  }, 
   test: {
     username: "root",
     password: null,
